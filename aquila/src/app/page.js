53f2f05df2f1ee2cf4f8a7c1431e0e01c867ec49
TIAGO AMERICANO RODRIGUES <tiago.americano.03@gmail.com>
2024-07-17 14:27:02 -0300

    first commit

---
+++ aquila/src/app/page.js
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Sua Peça É Original</title>
+        <meta name="description" content="Sua peça é original" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1>Sua peça é original</h1>
+      </main>
+    </div>
+  )
+}
